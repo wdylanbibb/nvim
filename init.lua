8e0f1f410695b12d3cd0cc532a3113a7cc1e6994
Dylan Bibb <wdylanbibb@pop-os.localdomain>
2022-05-04 21:13:33 -0400

    Credit for most of the config

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- A majority of this config was taken from
+-- https://github.com/mikebarkmin/.dotfiles/tree/main/nvim/.config/nvim
+-- both in the plugins used and the format.
+
 require('plugins')
 require('configs')
 require('keybindings')
